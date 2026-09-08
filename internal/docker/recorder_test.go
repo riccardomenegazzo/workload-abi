806c2edf4606db2c2f9f6e29174f00964d92e83d
Riccardo Menegazzo <52837869+riccardomenegazzo@users.noreply.github.com>
2026-09-09 01:04:45 +0200

    style: gofmt recorder tests

--- internal/docker/recorder_test.go
+++ internal/docker/recorder_test.go
@@ -4,10 +4,10 @@ import "testing"
 
 func TestParseBytes(t *testing.T) {
 	cases := map[string]int64{
-		"1B": 1,
-		"1KiB": 1024,
+		"1B":     1,
+		"1KiB":   1024,
 		"1.5MiB": 1572864,
-		"2GB": 2000000000,
+		"2GB":    2000000000,
 	}
 	for in, want := range cases {
 		if got := ParseBytes(in); got != want {
